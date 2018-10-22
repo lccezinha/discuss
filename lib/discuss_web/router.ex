a68128e5b7c8409790e18c27440bc1f3b3000fad
Luiz Cezer <lccezinha@gmail.com>
2018-10-22 15:38:50 -0300

    Create topic routes

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,6 +17,7 @@ defmodule DiscussWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
