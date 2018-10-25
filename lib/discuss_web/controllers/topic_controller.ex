bf9cc91edfda4249da633c432835837cf35a941f
Luiz Cezer <lccezinha@gmail.com>
2018-10-24 22:55:50 -0300

    Create create action to store a new topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,11 +1,19 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  alias Discuss.Topics
   alias Discuss.Topics.Topic
 
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, params)
 
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    {:ok, _} = Topics.create_topic(topic)
+
+    conn
+    |> redirect(to: topic_path(conn, :new))
   end
 end
--- test/discuss_web/controllers/topic_controller_test.exs
+++ test/discuss_web/controllers/topic_controller_test.exs
@@ -3,6 +3,21 @@ defmodule DiscussWeb.TopicControllerTest do
 
   test "GET /topics/new", %{conn: conn} do
     conn = get(conn, topic_path(conn, :new))
+
     assert html_response(conn, 200)
   end
+
+  test "POST /topics with valid data", %{conn: conn} do
+    params = %{title: "My Title"}
+
+    conn = post(conn, topic_path(conn, :create), topic: params)
+
+    assert redirected_to(conn) == topic_path(conn, :new)
+  end
+
+  # test "POST /topics with invalid data", %{conn: conn} do
+  #   conn = post(conn, topic_path(conn, :create))
+  #
+  #   assert response(conn, 200)
+  # end
 end
