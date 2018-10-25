a6914e294eeb8be30d670e05755849c9656113f7
Luiz Cezer <lccezinha@gmail.com>
2018-10-25 11:04:20 -0300

    Redirect to index when create new topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,12 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topics
   alias Discuss.Topics.Topic
 
+  def index(conn, _params) do
+    topics = Topics.list_topics()
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, params)
 
@@ -14,7 +20,7 @@ defmodule DiscussWeb.TopicController do
     case Topics.create_topic(topic) do
       {:ok, _} ->
         conn
-        |> redirect(to: topic_path(conn, :new))
+        |> redirect(to: topic_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
--- test/discuss_web/controllers/topic_controller_test.exs
+++ test/discuss_web/controllers/topic_controller_test.exs
@@ -4,6 +4,12 @@ defmodule DiscussWeb.TopicControllerTest do
   @valid_params %{title: "My Title"}
   @invalid_params %{title: ""}
 
+  test "GET /topics or /", %{conn: conn} do
+    conn = get(conn, topic_path(conn, :index))
+
+    assert html_response(conn, 200) =~ "Listing Topics"
+  end
+
   test "GET /topics/new", %{conn: conn} do
     conn = get(conn, topic_path(conn, :new))
 
@@ -13,7 +19,7 @@ defmodule DiscussWeb.TopicControllerTest do
   test "POST /topics with valid data", %{conn: conn} do
     conn = post(conn, topic_path(conn, :create), topic: @valid_params)
 
-    assert redirected_to(conn) == topic_path(conn, :new)
+    assert redirected_to(conn) == topic_path(conn, :index)
   end
 
   test "POST /topics with invalid data", %{conn: conn} do
