a61b60d2bed0c26898ac605c36f41b9fbf722d3b
Luiz Cezer <lccezinha@gmail.com>
2018-10-31 21:45:06 -0300

    Minimal setup for Ueberauth controller

---
+++ lib/discuss_web/controllers/auth/auth_controller.ex
@@ -0,0 +1,12 @@
+defmodule DiscussWeb.Auth.AuthController do
+  use DiscussWeb, :controller
+  plug Ueberauth
+
+  def callback(conn, params) do
+    IO.inspect("\n @@@@ ")
+    IO.inspect(conn.assigns)
+    IO.inspect("\n @@@@ ")
+    IO.inspect(params)
+    IO.inspect("\n @@@@ ")
+  end
+end
