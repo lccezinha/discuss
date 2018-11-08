ef3067df6c9fb35e4986ae78c2c4fa1576f2af0b
Luiz Cezer <lccezinha@gmail.com>
2018-11-07 22:44:05 -0200

    Finish github auth flow

--- lib/discuss_web/controllers/auth/auth_controller.ex
+++ lib/discuss_web/controllers/auth/auth_controller.ex
@@ -2,14 +2,29 @@ defmodule DiscussWeb.Auth.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect("\n @@@@ ")
-    IO.inspect(conn.assigns)
-    IO.inspect("\n @@@@ ")
-    IO.inspect(params)
-    IO.inspect("\n @@@@ ")
+  alias Discuss.Users
 
-    conn
-    |> resp(200, "{}")
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    signin(conn, auth)
+  end
+
+  defp signin(conn, auth) do
+    params = %{
+      email: auth.info.email,
+      name: auth.info.name,
+      token: auth.credentials.token
+    }
+
+    case Users.insert_or_find(params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User #{user.email} successful log in")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Some shit happen, login failed!")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 end
--- test/discuss_web/controllers/auth/auth_controller_test.exs
+++ test/discuss_web/controllers/auth/auth_controller_test.exs
@@ -1,10 +1,59 @@
 defmodule DiscussWeb.Auth.AuthControllerTest do
   use DiscussWeb.ConnCase
 
+  alias Discuss.Repo
+  alias Discuss.Users.User
+
+  @ueberauth_auth %{
+    credentials: %{
+      token: "mytoken123",
+    },
+    info: %{
+      email: "xunda@example.org",
+      name: "xunda",
+    }
+  }
+
+  @invalid_ueberauth_auth %{
+    credentials: %{
+      token: "",
+    },
+    info: %{
+      email: "xunda@example.org", 
+      name: "xunda",
+    }
+  }
+
   test "redirects user to github auth", %{conn: conn} do
     conn = get(conn, auth_path(conn, :request, "github"))
     
     assert redirected_to(conn, 302)
+  end
+
+  test "GET /:github with success auth must create new user", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:ueberauth_auth, @ueberauth_auth)
+      |> get("/auth/github/callback")
+
+    users = Repo.all(User)
+    assert Enum.count(users) == 1
+
+    assert get_flash(conn, :info) == "User xunda@example.org successful log in"
+    assert redirected_to(conn) == topic_path(conn, :index)
+  end
+
+  test "GET /:github with error auth must not create a new user", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:ueberauth_auth, @invalid_ueberauth_auth)
+      |> get("/auth/github/callback")
+
+    users = Repo.all(User)
+    assert Enum.count(users) == 0
+
+    assert get_flash(conn, :error) == "Some shit happen, login failed!"
+    assert redirected_to(conn) == topic_path(conn, :index)
   end
 end
 
